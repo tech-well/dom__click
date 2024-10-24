e1ff6d53f67d7d18b5085659f9330cfa1326b19a
wellington <wellingtoncarlos.tech@gmail.com>
2024-10-23 23:23:03 -0300

    click e duplo click para abrir conteudo e fechar

---
+++ script.js
@@ -0,0 +1,61 @@
+
+function soft__0() {
+    const skills = document.querySelector('.skills__0')
+    skills.style.height = '330px'
+    skills.style.background = 'url(imagens/pastor.jpeg)'
+    skills.style.backgroundSize = 'cover'
+
+    const skills__1 = document.querySelector('.skills__1') 
+    skills__1.style.height = '20px'
+    skills__1.style.background = 'none'
+
+    const skills__2 = document.querySelector('.skills__2') 
+    skills__2.style.height = '20px'
+    skills__2.style.background = 'none'
+
+
+
+}
+
+function out(){
+   const skills__0__out = document.querySelector('.skills__0')
+   skills__0__out.style.height = '20px'
+
+}
+
+
+
+function soft__1() {
+    const skills__1 = document.querySelector('.skills__1')
+    skills__1.style.height = '330px'
+    skills__1.style.background = 'url(imagens/pastor.jpeg)'
+    skills__1.style.backgroundSize = 'cover'
+    document.querySelector('.skills__0').style.height = '20px'
+    document.querySelector('.skills__0').style.background = 'none'
+    document.querySelector('.skills__2').style.height = '20px'
+    document.querySelector('.skills__2').style.background = 'none'
+
+}
+
+function out__1(){
+    const skills__1__out = document.querySelector('.skills__1')
+    skills__1__out.style.height = '20px'
+ 
+ }
+function soft__2() {
+    const skills__2 = document.querySelector('.skills__2')
+    skills__2.style.height = '330px'
+    skills__2.style.background = 'url(imagens/pastor.jpeg)'
+    skills__2.style.backgroundSize = 'cover'
+    document.querySelector('.skills__0').style.height = '20px'
+    document.querySelector('.skills__0').style.background = 'none'
+    document.querySelector('.skills__1').style.height = '20px'
+    document.querySelector('.skills__1').style.background = 'none'
+
+}
+
+function out__2(){
+    const skills__2__out = document.querySelector('.skills__2')
+    skills__2__out.style.height = '20px'
+ 
+ }
